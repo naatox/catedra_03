3a3c4220a36d8686af6d2d96c5c7f0d15a8c0124
nato <renato.morales@alumnos.ucn.cl>
2023-12-13 15:39:37 -0300

    feat: cat03

--- frontendMobileEV03/src/app/home/home.page.ts
+++ frontendMobileEV03/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../services/posts.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,34 +11,66 @@ export class HomePage implements OnInit {
   posts: any = [];
   comments: any = []
 
-  constructor(private PostsService: PostsService) { }
+  constructor(private PostsService: PostsService, private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {
     this.getPosts();
+
+
 
   }
 
   getPosts(){
     this.PostsService.getPosts().subscribe((res: any) => {
-      res.forEach((element: any) => {
-        this.PostsService.getComments().subscribe((commenta:any) => {
-          commenta.forEach((comment: any) => {
-            if (element.id == comment.postId){
-              element.comments = comment;
-              console.log(comment);
+
+      this.posts = res;
+      this.posts.forEach((element:any) => {
+        this.PostsService.getComments().subscribe((comments:any) => {
+          console.log(comments);
+          element.comments = [];
+          for (let i = 0; i < comments.length; i++) {
+            if (comments[i].postId == element.id) {
+
+              element.comments.push(comments[i]);
             }
-          });
+          }
+          console.log(this.posts.comments);
 
         });
 
       });
-      this.posts = res;
+
+
       console.log(this.posts);
+
     });
 
   }
 
-  deletePost(id: number){
+  async deletePost(id: number){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Estás seguro?',
+      buttons: [
+        {
+          text: 'Si',
+          role: 'confirm',
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    actionSheet.present();
+    if(await actionSheet.onWillDismiss()){
+      this.posts = this.posts.filter((post:any) => post.id !== id);
+
+    }else{
+    }
+
+
+
   }
 
 }
